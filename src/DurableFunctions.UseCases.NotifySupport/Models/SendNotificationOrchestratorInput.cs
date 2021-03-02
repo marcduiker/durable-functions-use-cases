94fdb933b78ac4cc7784399a07f6a4657267e25a
Marc Duiker <marcduiker@gmail.com>
2021-03-02 16:43:43 +0100

    remove space

--- src/DurableFunctions.UseCases.NotifySupport/Models/SendNotificationOrchestratorInput.cs
+++ src/DurableFunctions.UseCases.NotifySupport/Models/SendNotificationOrchestratorInput.cs
@@ -3,7 +3,7 @@ namespace DurableFunctions.UseCases.NotifySupport
     public class SendNotificationOrchestratorInput
     {
         public int MaxNotificationAttempts { get; set; }
-        public string  Message { get; set; }
+        public string Message { get; set; }
         public int NotificationAttemptCount { get; set; }
         public SupportContactEntity SupportContact { get; set; }
         public int WaitTimeForEscalationInSeconds { get; set; }
