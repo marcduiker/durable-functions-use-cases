382d7e3dcb7caac44baadd996fcaaeb04eefddfe
Marc Duiker <marcduiker@gmail.com>
2021-09-13 21:32:24 +0200

    Fix casing for serialized json records

--- FraudDetection/src/DurableFunctions.UseCases.FraudDetection/Models/AuditRecord.cs
+++ FraudDetection/src/DurableFunctions.UseCases.FraudDetection/Models/AuditRecord.cs
@@ -1,11 +1,22 @@
+using Newtonsoft.Json;
+
 namespace DurableFunctions.UseCases.FraudDetection.Models
 {
     public class AuditRecord
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+        
+        [JsonProperty("transaction")]
         public Transaction Transaction { get; set; }
+        
+        [JsonProperty("creditor")]
         public Customer Creditor { get; set; }
+        
+        [JsonProperty("debtor")]
         public Customer Debtor { get; set; }
+        
+        [JsonProperty("isSuspiciousTransaction")]
         public bool IsSuspiciousTransaction { get; set; }
     }
 }
--- FraudDetection/src/DurableFunctions.UseCases.FraudDetection/Models/Transaction.cs
+++ FraudDetection/src/DurableFunctions.UseCases.FraudDetection/Models/Transaction.cs
@@ -1,14 +1,26 @@
 using System;
+using Newtonsoft.Json;
 
 namespace DurableFunctions.UseCases.FraudDetection.Models
 {
     public class Transaction
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+        
+        [JsonProperty("creditorBankAccount")]
         public string CreditorBankAccount { get; set; }
+        
+        [JsonProperty("debtorBankAccount")]
         public string DebtorBankAccount { get; set; }
+        
+        [JsonProperty("amount")]
         public decimal Amount { get; set; }
+        
+        [JsonProperty("currency")]
         public string Currency { get; set; }
+        
+        [JsonProperty("timeStampUtc")]
         public DateTime TimeStampUtc { get; set; }
     
     }
